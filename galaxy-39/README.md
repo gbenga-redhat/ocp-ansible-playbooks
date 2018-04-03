d11e8f5f029df1649ed62b66a37bbd28380e6f35
gogs <gogs>
2018-04-03 15:35:42 -0400

    ocp 3.9 install

---
+++ galaxy-39/README.md
@@ -0,0 +1,8 @@
+roles for doing stuff to nodes...possibly aws nodes
+
+#oc_cluster_up
+create oc  3.9 cluster on single AWS RHEL node
+
+#oc_cluster_down
+tear down cluster on single AWS RHEL node
+TODO: combine the uninstall and install roles and just have a toggle flag
---
+++ galaxy-39/examples.sh
@@ -0,0 +1,14 @@
+#3.9
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts oc_cluster_up_aws.yml --extra-vars "rhn_user={{ lookup ('env', 'RH_USER') }} rhn_pass={{ lookup ('env', 'RH_PASS') }} rhn_pool_id={{ lookup ('env', 'RH_POOL') }} ocp_repos=rhel-7-server-ose-3.9-rpms cluster_name=ocp-cluster"
+
+#down
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts down_oc_cluster.yml
+
+#destroyCluster
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts destroy_oc_cluster.yml --extra-vars "cluster_name=ocp-cluster"
+
+#restart cluster
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts up_existing_cluster.yml --extra-vars "cluster_name=ocp-cluster"
+
+#tearDown
+ansible-playbook -u ec2-user --private-key $AWS_PEM_FILE  -i ./hosts tear_down_oc_cluster_aws.yml  --extra-vars "cluster_name=ocp-cluster"
